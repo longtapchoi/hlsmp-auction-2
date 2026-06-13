9887f8743f0c80f3a3dfbc4bef986e5680cdf9c2
longtapchoi <nongthienlongdmx@gmail.com>
2026-06-13 20:11:59 +0700

    Delete src/main directory

--- src/main/java/fr/maxlego08/zauctionhouse/api/storage/dto/TransactionDTO.java
+++
@@ -1,9 +0,0 @@
-package fr.maxlego08.zauctionhouse.api.storage.dto;
-
-import fr.maxlego08.zauctionhouse.api.transaction.TransactionStatus;
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.UUID;
-
-public record TransactionDTO(int id, UUID player_unique_id, String economy_name, BigDecimal before, BigDecimal after, BigDecimal value, TransactionStatus status, Date created_at, Date updated_at) {
-}
