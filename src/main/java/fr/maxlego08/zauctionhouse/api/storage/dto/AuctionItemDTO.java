e65e86200ee4d7ab29f00dab65db67a17b7883a5
longtapchoi <nongthienlongdmx@gmail.com>
2026-06-14 02:36:35 +0700

    Delete src/main directory

--- src/main/java/fr/maxlego08/zauctionhouse/api/storage/dto/AuctionItemDTO.java
+++
@@ -1,6 +0,0 @@
-package fr.maxlego08.zauctionhouse.api.storage.dto;
-
-import java.util.Date;
-
-public record AuctionItemDTO(int id, int item_id, String itemstack, Date created_at, Date updated_at) {
-}
