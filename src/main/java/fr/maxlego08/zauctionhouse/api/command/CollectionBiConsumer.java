e4bcaa880ff230a6eecdd5f371e69a5fd116a8e0
longtapchoi <nongthienlongdmx@gmail.com>
2026-06-14 03:31:41 +0700

    Delete src/main directory

--- src/main/java/fr/maxlego08/zauctionhouse/api/command/CollectionBiConsumer.java
+++
@@ -1,9 +0,0 @@
-package fr.maxlego08.zauctionhouse.api.command;
-
-import java.util.List;
-import org.bukkit.command.CommandSender;
-
-@FunctionalInterface
-public interface CollectionBiConsumer {
-   List<String> accept(CommandSender var1, String[] var2);
-}
