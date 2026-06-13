13d0818a55cfe0409bda5b5b30821a45be3460c0
longtapchoi <nongthienlongdmx@gmail.com>
2026-06-13 21:27:53 +0700

    Delete src/main directory

--- src/main/java/fr/maxlego08/zauctionhouse/api/configuration/ConfigurationFile.java
+++
@@ -1,5 +0,0 @@
-package fr.maxlego08.zauctionhouse.api.configuration;
-
-public interface ConfigurationFile {
-   void load();
-}
--- src/main/java/fr/maxlego08/zauctionhouse/libs/sarah/database/Executor.java
+++
@@ -1,9 +0,0 @@
-package fr.maxlego08.zauctionhouse.libs.sarah.database;
-
-import fr.maxlego08.zauctionhouse.libs.sarah.DatabaseConfiguration;
-import fr.maxlego08.zauctionhouse.libs.sarah.DatabaseConnection;
-import fr.maxlego08.zauctionhouse.libs.sarah.logger.Logger;
-
-public interface Executor {
-   int execute(DatabaseConnection var1, DatabaseConfiguration var2, Logger var3);
-}
