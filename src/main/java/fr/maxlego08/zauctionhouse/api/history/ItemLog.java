9e6470e9ba5c4975a14e36a3f4d288887c866883
longtapchoi <nongthienlongdmx@gmail.com>
2026-06-14 02:22:12 +0700

    Delete src/main directory

--- src/main/java/fr/maxlego08/zauctionhouse/api/history/ItemLog.java
+++
@@ -1,7 +0,0 @@
-package fr.maxlego08.zauctionhouse.api.history;
-
-import fr.maxlego08.zauctionhouse.api.item.Item;
-import fr.maxlego08.zauctionhouse.api.storage.dto.LogDTO;
-
-public record ItemLog(LogDTO log, Item item) {
-}
