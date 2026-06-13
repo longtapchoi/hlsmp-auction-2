6e8d0951079c69d7e26a75889949978b6022287f
longtapchoi <nongthienlongdmx@gmail.com>
2026-06-14 03:01:06 +0700

    Delete src/main directory

--- src/main/java/fr/maxlego08/zauctionhouse/api/placeholders/PlaceholderRegister.java
+++
@@ -1,7 +0,0 @@
-package fr.maxlego08.zauctionhouse.api.placeholders;
-
-import fr.maxlego08.zauctionhouse.api.AuctionPlugin;
-
-public interface PlaceholderRegister {
-   void register(Placeholder var1, AuctionPlugin var2);
-}
