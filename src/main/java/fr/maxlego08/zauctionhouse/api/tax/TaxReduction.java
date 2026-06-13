ff77882ab5719a6757d18e3f480d6cc4914ac2cc
longtapchoi <nongthienlongdmx@gmail.com>
2026-06-13 21:40:55 +0700

    Delete src/main directory

--- src/main/java/fr/maxlego08/zauctionhouse/api/tax/TaxReduction.java
+++
@@ -1,7 +0,0 @@
-package fr.maxlego08.zauctionhouse.api.tax;
-
-public record TaxReduction(String permission, double percentage) {
-   public double getMultiplier() {
-      return (double)1.0F - this.percentage / (double)100.0F;
-   }
-}
--- src/main/java/fr/maxlego08/zauctionhouse/libs/sarah/libs/hikari/metrics/MetricsTrackerFactory.java
+++
@@ -1,5 +0,0 @@
-package fr.maxlego08.zauctionhouse.libs.sarah.libs.hikari.metrics;
-
-public interface MetricsTrackerFactory {
-   IMetricsTracker create(String var1, PoolStats var2);
-}
